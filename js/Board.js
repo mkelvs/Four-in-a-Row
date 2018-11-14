9c78ae4c6c09b19398704854917e438125c63e8b
Mark Kelvin Libario <mkelvin.libario@gmail.com>
2018-11-14 02:07:10 -0500

    createSpace method on the Board class

--- js/Board.js
+++ js/Board.js
@@ -9,12 +9,10 @@ class Board {
    * Generates 2D array of spaces.
    * @return  {Array}     An array of space objects
    */
-
   createSpaces () {
     const spaces = []
     for (let x = 0; x < this.columns; x++) {
       const column = []
-
       for (let y = 0; y < this.rows; y++) {
         let space = new Space(x, y)
         column.push(space)
