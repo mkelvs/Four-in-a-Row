207c08a176301dd09e36a1e0e6f2e6b49f2c3607
Mark Kelvin Libario <mkelvin.libario@gmail.com>
2018-11-14 00:53:18 -0500

    added Player class properties
    Created the properties within the class Player

--- js/Player.js
+++ js/Player.js
@@ -1,3 +1,8 @@
 class Player {
-
+  constructor(id, name, tokenColor, isPlayerTurn = false) {
+    this.id = id;
+    this.name = name;
+    this.tokenColor = tokenColor;
+    this.isPlayerTurn = isPlayerTurn;
+  }
 }
